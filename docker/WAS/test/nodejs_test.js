df62c7e2a89e8f403cbbb78756fb1161c3133bc2
Jaehyuk-Lee <alex00728@outlook.com>
2024-04-17 11:24:33 +0900

    remove test

--- docker/WAS/test/nodejs_test.js
+++
@@ -1,8 +0,0 @@
-//nodejs_test.js
-var http = require('http');
-var content = function(req, res){
-  res.writeHead(200);
-  res.end("Welcome to JingO's Devlog!" + "\n");
-}
-var server = http.createServer(content);
-server.listen(8000);
