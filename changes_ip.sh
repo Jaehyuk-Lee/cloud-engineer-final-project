2987d37180e183199d5fa3f2a284b88e530478bf
ng2c <piert2588@gmail.com>
2024-04-24 17:48:52 +0900

    임시저장

---
+++ changes_ip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Vagrant IP 대역 변경
+new_private_network="192.168.111"
+sed -i "s/cfg\.vm\.network \"private_network\", ip: \"[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\./cfg.vm.network \"private_network\", ip: \"$new_private_network\./g" Vagrantfile
+
+# update_invetory.hosts.yaml IP 대역 변경
+sed -i "s/host_ip: \"[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\./host_ip: \"$new_private_network\./g" ansible/env/update_inventory_hosts.yaml
+sed -i "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.2/$new_private_network\.2/g" ansible/env/update_inventory_hosts.yaml
+sed -i "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.31/$new_private_network\.31/g" ansible/env/update_inventory_hosts.yaml
+sed -i "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.100/$new_private_network\.100/g" ansible/env/update_inventory_hosts.yaml
+
+# install_docker_nginx.yaml IP 대역 변경
+sed -i "s/was_ip: \"[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\./was_ip: \"$new_private_network\./g" ansible/web/install_docker_nginx.yaml
+
+# haproxy.cfg.j2 IP 대역 변경
+sed -i "s/server web-{{ \"%02d\"|format(i) }} \"[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\./server web-{{ \"%02d\"|format(i) }}!\"$new_private_network\./g" ansible/web/templates/haproxy.cfg.j2
