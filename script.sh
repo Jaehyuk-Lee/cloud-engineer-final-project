c0911f1975b10728ad1fc655eb1059cd045817d4
ng2c <piert2588@gmail.com>
2024-04-27 10:35:43 +0900

    서버 생성 및 hosts 자동화

--- script.sh
+++ script.sh
@@ -4,6 +4,11 @@
 end_number=${1:-1} # 인수가 없을 경우 기본값은 1
 j2_end_number=$((end_number + 1))
 
+if ! [[ $end_number =~ ^[0-9]+$ && $end_number -ge 1 && $end_number -le 10 ]]; then
+  echo "1부터 10 사이의 숫자를 입력해주세요"
+  exit 1
+fi
+
 sed -i 's/with_sequence: start=1 end=[0-9]\{1,2\}/with_sequence: start=1 end='"$end_number"'/g' ansible/env/update_inventory_hosts.yaml
 
 # 인벤토리 /etc/ansible/hosts 수정
